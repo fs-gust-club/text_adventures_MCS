c4449b4eda48f4a4e9f66301c22588a036cabda6
Mark Antill <mark.antill@countercept.com>
2019-10-30 18:33:40 +0000

    Copy from other repo

---
+++ src/main.rs
@@ -0,0 +1,83 @@
+#[macro_use]
+extern crate derive_new;
+
+#[macro_use]
+mod world_building;
+
+use std::io::{self};
+use crate::world_building::{World, Room, Item};
+
+fn main() {
+    let mut world = let_there_be_light();
+
+    println!("Welcome");
+
+    loop {        
+        // Write output
+        match world.get_player_room() {   
+            None => { 
+                println!("Location does not exist");
+                break;
+            },
+            Some(room) => {
+                println!("{}", room.get_full_description())
+            }
+        }
+
+        // Get input
+        let mut user_input = String::new();        
+        match io::stdin().read_line(&mut user_input) {            
+            Err(error) => {
+                println!("error: {}", error);
+                break;
+            },
+            _ => { }
+        }
+        
+        // Perform actions
+        match perform_action(&mut world, &user_input) {
+            Err(err) => {
+                println!("{}",err );
+                break;
+            },
+            Ok(output) => println!("{}", output)
+        }
+    }
+}
+
+fn let_there_be_light() -> World {
+    shaper_of_worlds!(
+        location = "A",
+        rooms = [
+            [
+                "A",
+                "This is A",
+                items = ["Stick", "Stone"],
+                exits = ["north" => "B" "west" => "c"]
+            ]
+            [
+                "B",
+                "This is B",
+                items = [],
+                exits = ["south" => "A"]
+            ]
+            [
+                "C",
+                "This is C",
+                items = [],
+                exits = ["east" => "A"]
+            ]
+        ]
+    )     
+}
+
+fn perform_action(world:&mut World, user_intput:&str) -> Result<String, String> {
+    let cased = user_intput.to_lowercase().trim().to_string();
+    match cased.as_ref() {
+        "exit" => Err("Exiting".to_string()),
+        _ => match world.move_player(&cased) {
+            Ok(message) => Ok(message),
+            Err(message) => Ok(message)
+        }
+    }
+}
---
+++ src/world_building.rs
@@ -0,0 +1,136 @@
+use std::collections::HashMap;
+use itertools::Itertools;
+
+#[derive(Default, Debug)]
+pub struct World {
+    pub locations: HashMap<String, Room>,
+    pub player_location: String,
+}
+
+impl World {
+    pub fn add_location(&mut self, location: Room) {
+        self.locations.insert(location.id.clone(), location);
+    }
+
+    pub fn move_player(&mut self, direction: &String) -> Result<String, String> {
+        // Check whether our current location has an exit that matches direction.
+        // If so, set the payers location to the pointed direction.
+        // returns a result with Ok(new_location), Err(No Exit)
+
+        let current_location = self.locations.get(&self.player_location).unwrap();
+
+        match current_location.exits.get(direction) {
+            Some(room_id) => {
+                self.player_location = room_id.clone();
+                Ok(format!("You have moved {}", direction))
+            }
+            None => Err(format!("{} is not a valid direction", direction)),
+        }
+    }
+
+    pub fn get_player_room(&mut self) -> Option<&Room> {
+        self.locations.get(&self.player_location)
+    }
+}
+
+#[derive(Debug, new)]
+pub struct Room {
+    pub id: String,
+    pub description: String,    
+    #[new(default)]
+    exits: HashMap<String, String>,
+    #[new(default)]
+    items: Vec<Item>,
+}
+
+impl<'a> Room {
+    pub fn add_exit(&mut self, command: String, exit_id: String) {
+        self.exits.insert(command, exit_id);
+    }
+    
+    pub fn get_exits(&self) -> impl Iterator<Item = &String> {
+        self.exits.keys()
+    }
+
+    pub fn get_item_names(&self) -> impl Iterator<Item = String> + '_ {
+        self.items.iter().map(|i| i.name.to_string())
+    }
+
+    pub fn add_item(&mut self, item: Item) {
+        self.items.push(item);
+    }
+
+    pub fn has_items(&self) -> bool {
+        self.items.len() > 0
+    }
+
+    pub fn get_full_description(&self) -> String {
+        let mut output = String::new();
+        let description = format!("\n{location_description}", location_description = self.description);
+        output.push_str(&description);
+            
+        let exits = format!("\nExits are {exits}", exits =self.get_exits().join(", "));
+        output.push_str(&exits);
+
+        if self.has_items() {            
+            let items = format!("\nItems are {item_names}", item_names = self.get_item_names().join(", "));
+            output.push_str(&items);
+        }
+
+        output
+    }
+
+    pub fn take_item(&mut self, player: &mut Player, item_name: String) -> Result<String, String> {
+        match self.items.iter().position(|i| i.name == item_name) {
+            Some(index) => {
+                let temp = self.items.remove(index);
+                player.inventory.push(temp);
+                Ok(format!("Picked up {}", item_name))
+            }
+            None => Err(format!("No item of type {} is present", item_name)),
+        }
+    }
+}
+
+#[derive(Default, Debug, new)]
+pub struct Player {
+    pub name: String,
+    #[new(default)]
+    pub inventory: Vec<Item>,
+}
+
+#[derive(Debug, new)]
+pub struct Item {
+    pub name: String,
+}
+
+macro_rules! shaper_of_worlds { 
+    (
+        location = $player_location:expr,
+        rooms = [
+            $([
+                $room_name:expr,
+                $room_description:expr, 
+                items=[$($item:expr$(,)*)*],
+                exits=[$($dir:expr => $dest:expr)*]   
+            ])+
+        ]
+    ) => {
+        {
+            let mut world = World::default();
+            world.player_location = $player_location.to_lowercase().to_string();
+            $(
+                let mut room = Room::new($room_name.to_lowercase().to_string(), $room_description.to_string());
+                $(
+                    let item = Item::new($item.to_string());
+                    room.add_item(item);
+                )*
+                $(
+                    room.add_exit($dir.to_string(), $dest.to_lowercase().to_string());
+                )*
+                world.add_location(room);
+            )+
+            world
+        }
+    };
+}
